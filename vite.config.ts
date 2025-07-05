53e53e2f006381e0da2637e027ecc45bea4c297f
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2025-07-06 01:12:53 +0300

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Overengineered-Todo-App/",
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle: () => {
+        copyFileSync('dist/index.html', 'dist/404.html');
+      }
+    }
+  ]
 })
